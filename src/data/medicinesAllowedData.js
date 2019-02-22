8e464e5c8a018e907aa43149fc736e3972c97e51
brenopolanski <breno.polanski@gmail.com>
2019-02-21 22:52:58 -0300

    Add new data for medicines allowed and not allowed

---
+++ src/data/medicinesAllowedData.js
@@ -0,0 +1,310 @@
+import uuid from 'uuid';
+
+export default [
+  {
+    id: uuid.v4(),
+    substanciaAtiva: 'Acetaminofeno',
+    categoria: 'Analgésico e Antipirético',
+    nomeComercial: ['Dôrico®', 'Tylenol®'],
+    fabricante: ['Sanofi Winthrop', 'Cilag']
+  },
+  {
+    id: uuid.v4(),
+    substanciaAtiva: 'Acetofenatidina',
+    categoria: 'Analgésico e Antipirético',
+    nomeComercial: [],
+    fabricante: []
+  },
+  {
+    id: uuid.v4(),
+    substanciaAtiva: 'Ácido Acetilsalicílico',
+    categoria: 'Analgésico e Antipirético',
+    nomeComercial: [
+      'AAS®',
+      'Aspirina®',
+      'Melhoral®',
+      'Sonrisal®',
+      'Ronal®',
+      'Buferin®',
+      'Doril®',
+      'Fontol®',
+      'Engov®'
+    ],
+    fabricante: [
+      'Sanofi Winthrop',
+      'Bayer',
+      'Sidney Ross',
+      'Rhodia',
+      'Bristol-Myers Squibb',
+      'Dorsay',
+      'Wyeth-whitehall',
+      'Newlab'
+    ]
+  },
+  {
+    id: uuid.v4(),
+    substanciaAtiva: 'Ácido p-aminobenzóico',
+    categoria: 'Anti-bacteriano',
+    nomeComercial: [],
+    fabricante: []
+  },
+  {
+    id: uuid.v4(),
+    substanciaAtiva: 'Aminopirina TAnti-reurnático',
+    categoria: '',
+    nomeComercial: '--I',
+    fabricante: []
+  },
+  {
+    id: uuid.v4(),
+    substanciaAtiva: 'A::Ii; -i ina',
+    categoria: 'Analgésico e Antipirético',
+    nomeComercial: 'Auditol®',
+    fabricante: 'Syndielabo-Espasil'
+  },
+  {
+    id: uuid.v4(),
+    substanciaAtiva: 'Vitamina C (Ácido Ascárbico)',
+    categoria: 'Vitamina Hidrossolúvel',
+    nomeComercial: 'Cebion®,Energil C®,Redoxon®,Cewin®Roche',
+    fabricante: 'Merck LegrandSanofi Winthrop'
+  },
+  {
+    id: uuid.v4(),
+    substanciaAtiva: 'Astemizol',
+    categoria: 'Anti-histamínico, antialérgico não sedante',
+    nomeComercial: 'Cilergil®,Hisnianal®,Hisnot®Farmasa',
+    fabricante: 'CilagJanssen F'
+  },
+  {
+    id: uuid.v4(),
+    substanciaAtiva: 'Azatadina',
+    categoria: 'Anti-histaminico, sedativo',
+    nomeComercial: 'Cedrin®',
+    fabricante: 'Schering'
+  },
+  {
+    id: uuid.v4(),
+    substanciaAtiva: 'Bronfeniramina',
+    categoria: 'Anti-histamínico',
+    nomeComercial: [],
+    fabricante: []
+  },
+  {
+    id: uuid.v4(),
+    substanciaAtiva: 'Cetirizina',
+    categoria: 'Anti-histaminico, antiasmático',
+    nomeComercial: 'Cetrizin®, Zetir®,Zyrtec®',
+    fabricante: 'Sintofarma AbbottGlaxo'
+  },
+  {
+    id: uuid.v4(),
+    substanciaAtiva: '',
+    categoria: 'Cloroguanidina',
+    nomeComercial: [],
+    fabricante: []
+  },
+  {
+    id: uuid.v4(),
+    substanciaAtiva: 'Cloroquina',
+    categoria: 'Antimalárico',
+    nomeComercial: 'Clopirim®, Cloroquina®',
+    fabricante: 'Quimioterápica Brasileira Bergamo'
+  },
+  {
+    id: uuid.v4(),
+    substanciaAtiva: 'Clorfeniramina (clorfenamina)',
+    categoria: 'Anti-histaminico',
+    nomeComercial: 'Benegrip®,Cheracap®,Resfry®,Termigripe C®Luper',
+    fabricante: 'NewlabRhodiaNeo-Química'
+  },
+  {
+    id: uuid.v4(),
+    substanciaAtiva: 'Ciproeptadina',
+    categoria: 'Anti-histamínico',
+    nomeComercial: 'Periatin®',
+    fabricante: 'Prodome'
+  },
+  {
+    id: uuid.v4(),
+    substanciaAtiva: 'Difenidramina',
+    categoria: 'Anti-histamínico',
+    nomeComercial: [],
+    fabricante: []
+  },
+  {
+    id: uuid.v4(),
+    substanciaAtiva: 'Dexclorfeniramina',
+    categoria: 'Anti-histamínico',
+    nomeComercial: 'Polaramine®, Histamin®',
+    fabricante: 'Schering-Plough Neo-Química'
+  },
+  {
+    id: uuid.v4(),
+    substanciaAtiva: 'Difenidramina',
+    categoria: 'Anti-histamínico',
+    nomeComercial: [],
+    fabricante: []
+  },
+  {
+    id: uuid.v4(),
+    substanciaAtiva: 'Doxorrubicina',
+    categoria: 'Antineoplásico',
+    nomeComercial:
+      'Adriblastina®, Doxorrubicina®, Doxorubicina®,Rubex®Biosintética',
+    fabricante: 'Pharmacia GüntherBristol-Myers Squibb'
+  },
+  {
+    id: uuid.v4(),
+    substanciaAtiva: '',
+    categoria: '',
+    nomeComercial: [],
+    fabricante: []
+  },
+  {
+    id: uuid.v4(),
+    substanciaAtiva: '',
+    categoria: '',
+    nomeComercial: [],
+    fabricante: []
+  },
+  {
+    substanciaAtiva: 'Substância Ativa',
+    categoria: 'Categoria',
+    nomeComercial: 'Nome Comercial',
+    fabricante: 'Fabricante'
+  },
+  {
+    substanciaAtiva: 'Elastina',
+    categoria: 'Anti-histamínico',
+    nomeComercial: '',
+    fabricante: ''
+  },
+  {
+    substanciaAtiva: 'Estreptomicina',
+    categoria: 'Antibiótico bacteriostático',
+    nomeComercial: '',
+    fabricante: []
+  },
+  {
+    substanciaAtiva: 'Fenacetina',
+    categoria: 'Analgésico e antipirético',
+    nomeComercial: '',
+    fabricante: []
+  },
+  {
+    substanciaAtiva: 'Fenilbutazona',
+    categoria: 'Antiinflamatório',
+    nomeComercial: 'Butazolidina®, Butazonil®',
+    fabricante: 'Novartis Teuto-Brasileiro'
+  },
+  {
+    substanciaAtiva: 'Fenitoína',
+    categoria: 'Antiepiléptico',
+    nomeComercial: 'Hidantal® Epelin®, Sereium®',
+    fabricante: 'Sarsa Aché Novaquímica'
+  },
+  {
+    substanciaAtiva: 'Hidroxizina',
+    categoria: 'Anti-histamínico',
+    nomeComercial: '',
+    fabricante: []
+  },
+  {
+    substanciaAtiva: 'Isoniazida',
+    categoria: 'Antibacteriano',
+    nomeComercial: 'Isoniazida®',
+    fabricante: 'União Química'
+  },
+  {
+    substanciaAtiva: 'L-Dopa',
+    categoria: 'Antiparkinsoniano .',
+    nomeComercial: 'Prolopa0,Cronomet®,Sinemet®Prodome',
+    fabricante: 'RocheMerck Sharp & Dohme'
+  },
+  {
+    substanciaAtiva: 'Loratadina',
+    categoria: 'Anti-histaminico',
+    nomeComercial: 'Claritin®, Loranil®',
+    fabricante: 'Schering-Plough Libbs'
+  },
+  {
+    substanciaAtiva: 'Mequitazina',
+    categoria: 'Anti-histamínico, antialérgico-',
+    nomeComercial: 'Primasone®',
+    fabricante: 'Rhodia'
+  },
+  {
+    substanciaAtiva: 'Oyatomkia',
+    categoria: 'Anti-histamink.)',
+    nomeComercial: '',
+    fabricante: '1'
+  },
+  {
+    substanciaAtiva: 'Para.-et:-.:niol',
+    categoria: 'Analgésico e A. f 1-: Ipir éfi c o',
+    nomeComercial:
+      'Cefaiex. ,Dôrico®,Tylenol®, Tylex®Dorilax®, Cefalium® Saridon®Aché Roche',
+    fabricante: 'GeyerSanofl Winthrop Cilag'
+  },
+  {
+    substanciaAtiva: 'Piramidona',
+    categoria: 'Analgésico e antipirético',
+    nomeComercial: '',
+    fabricante: []
+  },
+  {
+    substanciaAtiva: 'Pirimf..tamina',
+    categoria: 'Antimalárico',
+    nomeComercial: 'uár:.-prim®',
+    fabricante: 'Weilcome-Zeneca'
+  },
+  {
+    substanciaAtiva: 'Procaiiiamida',
+    categoria: 'Antiarritmic9',
+    nomeComercial: 'Procamide®',
+    fabricante: 'Zambon'
+  },
+  {
+    substanciaAtiva: 'Quinacrina',
+    categoria: 'Antimalárico',
+    nomeComercial: '',
+    fabricante: []
+  },
+  {
+    substanciaAtiva: 'Quinina',
+    categoria: 'Antimalárico',
+    nomeComercial: 'Paluquina®, Quinino®',
+    fabricante: 'Quimioterápica Brasileira Vital Brazil'
+  },
+  {
+    substanciaAtiva: 'Sulfisoxazole',
+    categoria: 'Sulfonamida',
+    nomeComercial: '',
+    fabricante: []
+  },
+  {
+    substanciaAtiva: 'Sulfadiazina',
+    categoria: 'Sulfonamida',
+    nomeComercial: 'Sulfadiazina®, Triglobe® Triglobe F®',
+    fabricante: 'Neo-Química Astra'
+  },
+  {
+    substanciaAtiva: 'Sulfametoxipiridazina',
+    categoria: 'Sulfonamida (antimalárico)',
+    nomeComercial: 'Nicosulfan®, Periodine®',
+    fabricante: 'Elofar  Simões'
+  },
+  {
+    substanciaAtiva: 'Terfenadina',
+    categoria: 'Anti-histamínico_',
+    nomeComercial: 'Fenasil®,Histadane®, Teldane®',
+    fabricante: 'SintofarmaCibranMerrell Lepetit'
+  },
+  {
+    substanciaAtiva: 'Trirnetoprima',
+    categoria: 'Antibacteriano',
+    nomeComercial: 'Bactrim®',
+    fabricante: 'Roche'
+  }
+];
---
+++ src/data/medicinesNotAllowedData.js
@@ -0,0 +1,321 @@
+import uuid from 'uuid';
+
+export default [
+  {
+    substanciaAtiva: 'Acetanilida',
+    categoria: 'Analgésico',
+    nomeComercial: [],
+    fabricante: []
+  },
+  {
+    substanciaAtiva: 'Acetilfenilhidrazina',
+    categoria: 'Antimicrobiano',
+    nomeComercial: [],
+    fabricante: []
+  },
+  {
+    substanciaAtiva: 'Ácido Nalidíxico',
+    categoria: 'Antibacteriano (quinolona)',
+    nomeComercial: 'Ácido Nalidíxico®, Nalurile,Wintomylon®',
+    fabricante: 'CibranCaziSanofi Winthrop'
+  },
+  {
+    substanciaAtiva: 'Ác.para-aminosalicílico',
+    categoria: 'Analgésico',
+    nomeComercial: [],
+    fabricante: []
+  },
+  {
+    substanciaAtiva: 'Ácido Úrico',
+    categoria: '',
+    nomeComercial: [],
+    fabricante: []
+  },
+  {
+    substanciaAtiva: 'Alfa-metildona',
+    categoria: '',
+    nomeComercial: [],
+    fabricante: []
+  },
+  {
+    substanciaAtiva: 'Azul de Metileno',
+    categoria: '',
+    nomeComercial: [],
+    fabricante: []
+  },
+  {
+    substanciaAtiva: 'Azul de Toluidina',
+    categoria: '',
+    nomeComercial: [],
+    fabricante: []
+  },
+  {
+    substanciaAtiva: 'Captopril',
+    categoria: 'Anti-Hinertensivo',
+    nomeComercial: 'Capoten0. Lopril -D® Captoprii(, CatoproL, Hipocatril0',
+    fabricante: 'Bristol Myers Squibb Berqarno e 1,1co-Quimil.a 1-los5onCibran'
+  },
+  {
+    substanciaAtiva: 'Cloranfenicol',
+    categoria: 'Antimicrobiano',
+    nomeComercial:
+      'Cloranfenicol®, Quemicetina®,Farmicetina OSintomicetina0I.Q.C.',
+    fabricante: 'Eurofarma,Neo-Química, TeutoBrasileiro,Carlo Erba'
+  },
+  {
+    substanciaAtiva: 'Ciprofloxacino',
+    categoria: 'Antibacteriano knuorquinoiona)',
+    nomeComercial:
+      'Biamoti10,rifloxe,Cifloxina®,Ciloxan®,Cipro®,Ciprofloxacina®,PROCINO,Quinoflox®Alcon Bayer',
+    fabricante: 'Frumtost AchéBergamoBiosintéticaSchering-Plough União Química'
+  },
+  {
+    substanciaAtiva: 'Co-trimoxazol',
+    categoria: 'Bactericida',
+    nomeComercial: [],
+    fabricante: []
+  },
+  {
+    substanciaAtiva: 'Dapsona',
+    categoria: 'Sulfona',
+    nomeComercial: 'Dapsona0, Sulfon®',
+    fabricante: 'Sanval Brasifa'
+  },
+  {
+    substanciaAtiva: 'Diamino-difenilsulfona',
+    categoria: 'Sulfona',
+    nomeComercial: [],
+    fabricante: []
+  },
+  {
+    substanciaAtiva: 'Dimercaprol',
+    categoria: '',
+    nomeComercial: [],
+    fabricante: []
+  },
+  {
+    substanciaAtiva: 'Enalapril (maleato)',
+    categoria: 'Anti-Hipertensivo',
+    nomeComercial: 'Atens®,Eupressin®, Re:!itec®,',
+    fabricante: 'FarmasaBiosintéticaMerck, Sharp & Dohme'
+  },
+  {
+    substanciaAtiva: 'Fenil-hidrazina',
+    categoria: 'Hemolitico',
+    nomeComercial: [],
+    fabricante: []
+  },
+  {
+    substanciaAtiva: 'Fluotane',
+    categoria: 'Analgésico',
+    nomeComercial: [],
+    fabricante: 'Wyeth'
+  },
+  {
+    substanciaAtiva: 'Furaltadona',
+    categoria: 'Antibacteriano (nitrofurano)',
+    nomeComercial: [],
+    fabricante: []
+  },
+  {
+    substanciaAtiva: 'Furazolidona',
+    categoria: 'Antibacteriano (nitrofurano)',
+    nomeComercial: 'G1ARLAM®',
+    fabricante: 'UCI-Farma'
+  },
+  {
+    substanciaAtiva: 'Funnetonol',
+    categoria: 'Bactericida',
+    nomeComercial: [],
+    fabricante: []
+  },
+  {
+    substanciaAtiva: 'Halotano',
+    categoria: 'Anestésico',
+    nomeComercial: 'Fluothane Halothane Halotano',
+    fabricante: 'Welcome-ICI WyethHoechst'
+  },
+  {
+    substanciaAtiva: 'Substância Ativa',
+    categoria: 'Categoria',
+    nomeComercial: 'Nome Comercial',
+    fabricante: 'Fabricante'
+  },
+  {
+    substanciaAtiva: 'Hidralazina',
+    categoria: 'Anti-hipertensivo',
+    nomeComercial: 'Apresoiina0,Hidralazina®,Lowpress®,Nepresol®Cazi Cristália',
+    fabricante: 'Novartis SanvalC'
+  },
+  {
+    substanciaAtiva: 'Hidrocloreto de Tolueno',
+    categoria: '',
+    nomeComercial: [],
+    fabricante: []
+  },
+  {
+    substanciaAtiva: 'Hidroxicloroquina',
+    categoria: 'Antimalárico',
+    nomeComercial: 'Plaquinol®',
+    fabricante: 'Sanofi Winthrop'
+  },
+  {
+    substanciaAtiva: 'Metamizol_',
+    categoria: 'Analgésico e Antipirético1',
+    nomeComercial:
+      'Anador®,Analgex0,BaralginO,Conmel®,Debela®,Dipirona®,Doran®,Doril®,Novalgina®SarsaBrasifa',
+    fabricante:
+      'Boehring De Angeli União QuímicaSanofi WintthropBergamo, Cazi, EMS, NeoQuímica, Teuto-Brasileiro, VitalBrazil,Q:I.FDorsay Hocchst'
+  },
+  {
+    substanciaAtiva: 'Mestranol',
+    categoria: 'Estrog',
+    nomeComercial: [],
+    fabricante: []
+  },
+  {
+    substanciaAtiva: 'Naftaleno',
+    categoria: '',
+    nomeComercial: [],
+    fabricante: []
+  },
+  {
+    substanciaAtiva: 'Neoarsfenamina',
+    categoria: 'Bactericida',
+    nomeComercial: [],
+    fabricante: []
+  },
+  {
+    substanciaAtiva: 'Nitrofurantoína',
+    categoria: 'Antibacteriano (nitrofurano)',
+    nomeComercial: 'Macrodantina®, Urogen®',
+    fabricante: 'Schering-Plough Prodotti'
+  },
+  {
+    substanciaAtiva: 'Nitrofurazona',
+    categoria: 'Antibacteriano (nitrofurano)',
+    nomeComercial: 'Furacin®,Alivinderm®,CazidennO,Nitrofurazona®Cazi',
+    fabricante: 'Schering-Plough O.I.F.União Química, Veafarm'
+  },
+  {
+    substanciaAtiva: 'Norfloxacino',
+    categoria: 'Antibacteriano (fluorquinolona)',
+    nomeComercial:
+      'Floxacin®, Chibroxin®, Floxinole ,Noracin®,Quinoform®,Respexil®,Uritrat NFO,Uroflox0,Uroplex®Cibran E M SProdome',
+    fabricante: 'Merck Sharp & Dohme Millet Roux LibbsFarmionSintofarma'
+  },
+  {
+    substanciaAtiva: '',
+    categoria: 'Niridazole',
+    nomeComercial: [],
+    fabricante: []
+  },
+  {
+    substanciaAtiva: 'Ofloxacino',
+    categoria: 'Antibacteriano (fluorquinolona)',
+    nomeComercial: 'Floxstat®, Oflox®,Ofloxan',
+    fabricante: 'Cilaaz,Allergan-Lok Janssen'
+  },
+  {
+    substanciaAtiva: 'Pamaquina',
+    categoria: 'Antimalárico',
+    nomeComercial: [],
+    fabricante: []
+  },
+  {
+    substanciaAtiva: 'Pentaquina',
+    categoria: 'Antimalárico',
+    nomeComercial: [],
+    fabricante: []
+  },
+  {
+    substanciaAtiva: 'Piridium',
+    categoria: '',
+    nomeComercial: [],
+    fabricante: []
+  },
+  {
+    substanciaAtiva: 'Primaquina',
+    categoria: 'Antimalárico',
+    nomeComercial: 'Primaquin®Primaquina®Primaquina Kinder®',
+    fabricante: 'FarrnoquimicaEndoterápica do Brasil Kinder'
+  },
+  {
+    substanciaAtiva: 'Substância Ativa',
+    categoria: 'Categoria',
+    nomeComercial: 'Nome Comercial',
+    fabricante: 'Fabricante'
+  },
+  {
+    substanciaAtiva: 'Probenicida',
+    categoria: 'Agente antigota-',
+    nomeComercial: 'Probecilin®, Gonocilin®, Degona®',
+    fabricante: 'EurofarmaUnião Química Prodotti'
+  },
+  {
+    substanciaAtiva: 'Quinidina',
+    categoria: 'Antiarrítmico',
+    nomeComercial: 'Natisedine®, Quinicardine® Quinidine Duriles®',
+    fabricante: 'Barrenne Astra'
+  },
+  {
+    substanciaAtiva: 'Quinocida',
+    categoria: 'Antimalárico',
+    nomeComercial: [],
+    fabricante: []
+  },
+  {
+    substanciaAtiva: 'Sulfacetamida',
+    categoria: 'Antibacteriano (sulfonamida)',
+    nomeComercial: 'Uromix®, Vagi-Sulfa0',
+    fabricante: 'Breves Cilag'
+  },
+  {
+    substanciaAtiva: 'Sulfametoxazol',
+    categoria: 'Antibacteriano (sulfonamida)',
+    nomeComercial: 'Bacteracin®, Bactrim®,Infectrin0, Neotrin®',
+    fabricante: 'Teuto-BrasileiroRacheBoehring De Angeli Neo-Química'
+  },
+  {
+    substanciaAtiva: 'Sulfametoxipirimidina',
+    categoria: 'Antibacteriano /sulfonamida',
+    nomeComercial: [],
+    fabricante: []
+  },
+  {
+    substanciaAtiva: 'Sulfanilamida',
+    categoria: 'Anti-séptico',
+    nomeComercial: 'Sulfanilamida Pó Medic®',
+    fabricante: 'Medic'
+  },
+  {
+    substanciaAtiva: 'Sulfapiridina',
+    categoria: 'Antibacteriano',
+    nomeComercial: [],
+    fabricante: []
+  },
+  {
+    substanciaAtiva: 'Sulfasalazina',
+    categoria: 'Antibacteriano/sulfonamida',
+    nomeComercial: [],
+    fabricante: []
+  },
+  {
+    substanciaAtiva: 'TrimtrGto)Jeno...._,',
+    categoria: '',
+    nomeComercial: [],
+    fabricante: []
+  },
+  {
+    substanciaAtiva: 'Urdo Oxiiase',
+    categoria: '',
+    nomeComercial: 'I',
+    fabricante: []
+  },
+  {
+    substanciaAtiva: 'Vitamina K',
+    categoria: '',
+    nomeComercial: [],
+    fabricante: []
+  }
+];
